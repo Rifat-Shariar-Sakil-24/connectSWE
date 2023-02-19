98e97fdbcc64f16c6183030e6a454d0603b70e0e
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2023-02-19 20:36:50 +0600

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
-import 'package:connectswe/Screens/SignInSignUp/signinsignup.dart';
+import 'package:connectswe/screen/login_signup.dart';
 import 'package:flutter/material.dart';
 
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(LoginSignupUI());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class LoginSignupUI extends StatelessWidget {
+  // const LoginSignupUI({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-     home: SignInSignUp()
-
+      debugShowCheckedModeBanner: false,
+      title: "Login Signup UI",
+      home: LoginSignupScreen(),
     );
   }
 }
